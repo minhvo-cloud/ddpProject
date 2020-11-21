4ba4594a6c4fc1611c0a152f344d00928c4665b5
Minh Vo <minh.vo4@gmail.com>
2020-11-21 14:59:35 -0600

    Second commit

--- optionCalculator1/server.R
+++ optionCalculator1/server.R
@@ -1,17 +1,9 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
 
 library(shiny)
 library(ragtop)
 library(car)
 
-# Define server logic required to draw a histogram
+# Define server logic required to calculate option price
 shinyServer(function(input, output) {
         value <- reactiveValues()
         
--- optionCalculator1/ui.R
+++ optionCalculator1/ui.R
@@ -1,19 +1,11 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
 
 library(shiny)
 
-# Define UI for application that draws a histogram
+# Define UI for application that computes option price
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Option Calculator"),
+  titlePanel("Financial Option Calculator"),
   
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
